e92fd3ce983241f376fcf82a512d664217dda915
awen-li <li.wen@wsu.edu>
2025-12-19 10:30:46 -0800

    add gdriver

--- gdriver/gdriver/__main__.py
+++ gdriver/gdriver/__main__.py
@@ -5,8 +5,6 @@ from .drivergen import DriverGenerator
 def main():
     parser = argparse.ArgumentParser(description="Option-based Driver Generator")
     parser.add_argument("binary_path", help="Path to the binary directory (e.g., ./libxml2/xmllint)")
-    parser.add_argument("--binary", help="Binary name within the benchmark (e.g., xmllint)")
-
     args = parser.parse_args()
 
     if not args.binary_path:
@@ -14,10 +12,9 @@ def main():
         return
 
     gen = DriverGenerator(args.binary_path)
-    if args.gen:
-        if not gen.checkCached():
-            gen.generate()
-        gen.dump_driver_cmdlines()
+    if not gen.checkCached():
+        gen.generate()
+    gen.dump_driver_cmdlines()
 
 if __name__ == "__main__":
     main()
